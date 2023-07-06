cb4bbbb394705587a6422119dcbde2246779b0ea
Thomas <0milieux_member@icloud.com>
2023-07-06 15:53:46 +0200

    Initial commit after initializing the project.

---
+++ dicopal/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for dicopal."""
+from dicopal.utils.config import config
+from dicopal.utils.logger import logger
+
+__all__ = ["config", "logger"]
+__author__ = """Thomas Camminady"""
+__email__ = "0milieux_member@icloud.com"
+__version__ = "0.1.0"
---
+++ tests/test_dicopal.py
@@ -0,0 +1,8 @@
+"""Tests for `dicopal` package."""
+
+
+from dicopal import __version__
+
+
+def test_version() -> None:
+    assert __version__ == "0.1.0"
